bfcb380c920e0697ade1ad66c11924b2acd89094
Elsbeth <esites@healthleadsusa.org>
2020-07-16 16:37:20 -0700

    compiled the 2 dataframes

--- create_lists.R
+++ create_lists.R
@@ -22,6 +22,18 @@ county_names_nw <- County_PIT_data_links_nw$County
 # map extract_pit() over the objects, one for each type of csv 
 compiled_df1 <- map2_dfr(location_url_1, county_names_1, extract_pit) # this works! wow
 
-# write to csv
-write.csv(compiled_df1, "Compiled PIT data.csv")
+# read in supplementary data
+extra_PIT_data <- read.csv("extra_PIT_data.csv", fileEncoding="UTF-8-BOM")
 
+# rowbind the two dataframes
+final_df <- rbind(compiled_df1, extra_PIT_data)
+
+Total_homeless_by_CoC <- final_df %>%
+  filter(pop_category == "Total Homeless Persons") %>%
+  select(CoC, emergency_shelter, transitional_housing, unsheltered)
+
+
+# come back to this later, making a viz to check if everything's working okay
+Total_homeless_by_CoC_chart <- ggplot(Total_homeless_by_CoC, aes(x=CoC)) + geom_bar()
+
+long_Total_homeless_by_CoC  <- Total_homeless_by_CoC %>% gather(Quarter, Revenue, Qtr.1:Qtr.4)
